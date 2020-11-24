fb27a531261ee9adc855210b445d8143df7c6dab
Ruben <rub_ben_san@hotmail.com>
2020-11-24 22:23:45 +0100

    "hola,mundo" tercera version copiando

--- JavaApplication3/src/javaapplication3/JavaApplication3.java
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -17,6 +17,9 @@ public class JavaApplication3 {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
         System.out.println("===========");
+        
+         System.out.println("Hola mundo");
+        System.out.println("===========");
     }
     
 }
